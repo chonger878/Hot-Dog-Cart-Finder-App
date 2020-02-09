2781cad335f83da42b47f83f9d369192b1859ef7
mxwllndrsn <mxwllndrsn@gmail.com>
2020-02-08 23:45:15 -0800

    test database integration successful

---
+++ routes/db.js
@@ -0,0 +1,24 @@
+/* MySQL CONNECTION
+ * HotDog.sql >> 'hotDog' DATABASE REQUIRED
+ * Run as root
+ * Password root
+ * Port 3306
+ */
+
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'hotDog'
+});
+
+db.connect(function(err){
+    if(err){
+        console.error('error on connection: '+err.stack);
+        return;
+    }
+    console.log('connection on '+db.threadId);
+});
+
+module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,24 @@ var express = require('express');
 var router = express.Router();
 var db = require('./db');
 
-var queryStr = `SELECT Customers.FirstName, Customers.CustomerID \n
-FROM Customers
-   JOIN Orders USING(CustomerID) \n
-WHERE CustomerID > 2;`;
+var queryStr = `SHOW TABLES`;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
-  var dbReturn = db.query(queryStr, 
+  db.query(queryStr, 
     function(error, results, fields){ 
         if(error) throw error;
         
         console.log(results[0]);
-        results[0]});
+        var dbReturn = results;
 
-  res.render('index', 
-    { 
-      title: 'Express',
-      body: dbReturn });
+        res.render('index', 
+        { 
+          title: 'Express',
+          dbReturn
+        });
+    });
 });
 
 
