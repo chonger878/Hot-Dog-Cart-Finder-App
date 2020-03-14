c2c266e28e435d2b05ac0eaf73c4c87737c0305b
Jack <jackb116@aol.com>
2020-03-13 22:14:12 -0700

    adding styling/security changes

--- front-end-react/src/components/signup/SignUp.js
+++ front-end-react/src/components/signup/SignUp.js
@@ -23,6 +23,13 @@ class SignUp extends React.Component {
     //inputs user form data to the database as long as the data is valid
     handleSubmit(e) {
          e.preventDefault();
+         //Trigger in database to only accept passwords longer than 8 chars and alphanum+special char
+         //email address must have @
+         if(this.refs.Password.value.length < 8)
+         {
+            alert("Password must be longer than 8 characters");
+            document.userData.Password.focus();
+         }
          var self = this; //assign self instead of this for body below
          this.setState({count: this.state.count + 1});
          //this.validate();
@@ -48,19 +55,21 @@ class SignUp extends React.Component {
      render() {
        return (
         <div className="SignUp">
-           <h1> Sign Up</h1>
+           <h1 className ="head"> Sign Up</h1>
            <img src = {profile} className = "profile" alt="loading"/>
           <form name="userData" onSubmit={this.onSubmit}>
-           ID:         <input type="text" value={this.state.count} ref="SigninId"/> {/*id will always be valid using state.count*/ }
+          
+           ID: <input type="text" value={this.state.count} ref="SigninId"/> {/*id will always be valid using state.count*/ }
            Permission: <input type="text" value="None" ref="Permission"/> {/*always none*/ }
+           
            Type:          <select ref="Type"> {/*type should be either vendor or customer, not admin*/ }
                             <option value="Vendor"  >Vendor</option>
                             <option value="Customer">Customer</option>
                           </select> 
-           Email:     <input type="text"  ref="Email"/> {/*change type to email for easier validation*/ }
-           Password:  <input type="text"  ref="Password"/>
-           FirstName: <input type="text"  ref="FirstName"/>
-           LastName:  <input type="text"  ref="LastName"/>
+           Email:<input type="email" name="Email" ref="Email"/> {/*change type to email for easier validation*/ }
+           Password:<input type="password" name="Password" ref="Password"/>
+           First Name:<input type="text"  ref="FirstName"/>
+           Last Name:<input type="text"  ref="LastName"/>
 
            <input type="submit" value="Submit" />
          </form>
